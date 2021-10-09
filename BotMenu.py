fdd3208ee752c0635e89c01ba84b4cd628e07d49
Poux7 <polo50210@gmail.com>
2021-10-09 14:52:53 +0200

    Merge branch 'main' of https://github.com/SourcierDeVerite/BotMenu.py

--- BotMenu.py
+++ BotMenu.py
@@ -96,7 +96,7 @@ bot = commands.Bot(command_prefix=";")
 async def on_ready():
 	print("\nLe bot est connecté! \n")
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=';menu'))
-	print("Salut Paul")
+
 
 @bot.command(name="ping")
 async def ping(ctx):
---
+++ startbotmenu.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+cd "C:\Users\Paul-Samuel\Documents\Charles\BotMenu.py"
+
+nodemon C:\Users\Paul-Samuel\Documents\Charles\BotMenu.py\BotMenu.py
