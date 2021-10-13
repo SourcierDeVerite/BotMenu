ee6b9fbe7e2a756f4c743dc09f9f8b9db9e4600d
Charles <lefranccharlespro@gmail.com>
2021-10-13 14:13:29 +0200

    V1.2 (Ajout des /commandes)

--- BotMenu.py
+++ BotMenu.py
@@ -1,14 +1,18 @@
+# https://bit.ly/3mIYH9b
 # Importation des modules
 import discord
 import datetime
 import time
 import os
 import urllib.request
+import mysql.connector
 
-from dotenv import load_dotenv
-from bs4 import BeautifulSoup
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option,create_choice
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 # Recupérer la date
 def voirdate ():
@@ -50,6 +54,8 @@ def scrapping():
 	# Transformer le code source en str
 	codesource = str(codesource)
 
+	date ="5 octobre 2021"
+
 	# Séparer la date que l'on veux 
 	debut = codesource.find(date)
 	codesource = codesource[debut:]
@@ -81,39 +87,54 @@ def scrapping():
 	finpizza = codesource.find("Desserts")
 	pizza = codesource[debutpizza:finpizza]
 
-	# Enlever les intitulés des différentes parties
 	plat = plat[5:]
 	accompagnement = accompagnement[15:]
 	pizza = pizza[19:]
 
+# Creer un embed simple
+def embedsimple(message):
+	embed = discord.Embed(title=message, color=discord.Color.red())
+	embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+	embed.set_footer(text="Crée par Sourcier De Vérité#1962") 
+
+	return embed
+
+# Creer un embed pour le menu
+def embedmenu(plat,accompagnement,pizza,date):
+	embed = discord.Embed(title="Le menu d'aujourd'hui est :", description=date,color=discord.Color.red())
+	embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+	embed.add_field(name="Plat", value=plat, inline=False)
+	embed.add_field(name="Accompagnement", value=accompagnement, inline=False)
+	embed.add_field(name="Pizza", value=pizza, inline=False)
+	embed.set_footer(text="Crée par Sourcier De Vérité#1962")
+	return embed
+
 # Récupérer le Token dans le fichier config.env
 load_dotenv(dotenv_path="config.env")
 
-# Définir le préfixe du Bot
-bot = commands.Bot(command_prefix=";")
+# Initialiser le bot
+bot = commands.Bot(command_prefix=":", help_command=None)
+slash = SlashCommand(bot, sync_commands=True)
+guild_ids=[715926570795008010,872202739046830080,763456598697705472,887273660254138419]
+#guild_ids=[715926570795008010]
 
 @bot.event
 async def on_ready():
-	print("\nLe bot est connecté! \n")
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=';menu'))
+	print("\nLe bot est connecté ! \n")
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='/menu'))
 
-@bot.command(name="ping")
+@slash.slash(name="Ping", description="Envoie le ping du bot", guild_ids=guild_ids)
 async def ping(ctx):
 	print("Un utilisateur à utilisé la commande Ping")
-	await ctx.message.delete()
 
-	message = f"Mon ping est de " + str(round(bot.latency * 1000))
+	message = "Mon ping est de " + str(round(bot.latency * 1000))
+	embed = embedsimple(message)
 
-	embed = discord.Embed(title=message, color=discord.Color.red())
-	embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+	await ctx.reply(embed=embed)
 
-	await ctx.send(embed=embed)
-
-@bot.command(name="menu")
+@slash.slash(name="Menu", description="Envoie le menu du RU", guild_ids=guild_ids)
 async def menu(ctx):
-
 	print("Un utilisateur à utilisé la commande Menu")
-	await ctx.message.delete()
 
 	voirdate()
 	scrapping()
@@ -126,44 +147,34 @@ async def menu(ctx):
 			vide = 1
 			break
 	if vide == 0 : 
-		embed = discord.Embed(title="Désolé, mais le Menu n'est pas affiché sur le site", color=discord.Color.red())
-		embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
-
+		message = "Désolé, mais le Menu n'est pas affiché sur le site"
+		embed = embedsimple(message)
 		await ctx.send(embed=embed)
 	else :
-		# Créer l'embed
-		embed = discord.Embed(title="Le menu d'aujourd'hui est :", description=date,color=discord.Color.red())
-		embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
-		embed.add_field(name="Plat", value=plat, inline=False)
-		embed.add_field(name="Accompagnement", value=accompagnement, inline=False)
-		embed.add_field(name="Pizza", value=pizza, inline=False)
-		embed.set_footer(text="Crée par Sourcier De Vérité#1962") 
+		embed = embedmenu(plat, accompagnement, pizza, date)
 		await ctx.send(embed=embed)
 
-@bot.command(name="sup")
+@slash.slash(name="sup", description="Supprimer des messages", guild_ids=guild_ids, options = [create_option(name="nombre",description="Nombre de message à supprimer",required=True,option_type=4)])
 @commands.has_permissions(manage_messages=True)
 async def sup(ctx, nombre : int):
-	await ctx.message.delete()
 	print(f"Un utilisateur à utilisé la commande Supprimer pour {nombre} message(s)")
 	
 	await ctx.channel.purge(limit = nombre)
-	
-	message = f"J'ai bien supprimé {nombre} message(s)"
-	embed = discord.Embed(title=message, color=discord.Color.red())
-	embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+	message = "J'ai bien supprimé " + str(nombre) + " message(s)"
+	embed = embedsimple(message)
+
 	await ctx.send(embed=embed,delete_after=5)
 
-@bot.command(name="ban")
+@slash.slash(name="ban", description="Bannir un membre", guild_ids=guild_ids, options = [create_option(name="membre",description="Membre à ban",required=True,option_type=6)])
 @commands.has_permissions(administrator=True)
-async def ban(ctx, member : discord.Member, reason=None):
-	await ctx.message.delete()
-	print(f"Un utilisateur à utilisé le commande Ban pour l'utilisateur {member}")
+async def ban(ctx, membre, reason=None):
+	print(f"Un utilisateur à utilisé le commande Ban pour l'utilisateur {membre}")
 
-	message = f"Le membre {member} a bien été banni"
-	embed = discord.Embed(title=message, color=discord.Color.red())
-	embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+	await membre.ban(reason = reason)
 
-	await member.ban(reason = reason)
+	message = "Le membre " + str(membre) + " a bien été banni"
+	embed=embedsimple(message)
+
 	await ctx.send(embed=embed)
 
 @sup.error
@@ -171,8 +182,7 @@ async def ban(ctx, member : discord.Member, reason=None):
 async def error(ctx, error):
 	if isinstance(error, MissingPermissions):
 		message = "Vous n'avez pas la permsision de faire ça"
-		embed = discord.Embed(title=message, color=discord.Color.red())
-		embed.set_author(name="Crous", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b5/Crous_logo.jpg")
+		embed = embedsimple(message)
 
 		await ctx.send(embed=embed)
 
